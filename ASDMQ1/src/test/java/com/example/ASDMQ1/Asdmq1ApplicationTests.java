984853ebf6ac2e956a83a50fe507b63aad740bc4
Tabassum <tabs22sultana@gmail.com>
2019-06-15 12:02:46 +0530

    updated folder1git add --all!

---
+++ ASDMQ1/src/test/java/com/example/ASDMQ1/Asdmq1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.ASDMQ1;
+
+import org.testng.annotations.Test;
+
+public class Asdmq1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Junit converted  into the testng!!");
+	}
+
+}
